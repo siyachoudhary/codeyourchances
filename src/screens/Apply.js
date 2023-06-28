ac1e724f5f1659813b013e4522cc5e3f6927d511
Siya Choudhary <Siyacutesiya@gmail.com>
2023-06-28 10:45:12 -0500

    apply page complete

--- src/screens/Apply.js
+++ src/screens/Apply.js
@@ -3,7 +3,10 @@ import AOS from "aos";
 import "./Apply.css"
 
 export const Apply = () => {
-  
+  useEffect(()=>{
+    AOS.init({duration: 500, easing:"ease-in-out"});
+  }, []);
+
     return (
       <div className="containerApply">
         
@@ -19,14 +22,15 @@ export const Apply = () => {
             <div><img src={process.env.PUBLIC_URL + "/images/apply2.webp"}></img></div>
           </div>
           <br></br>
-          <h3 className='blueHeading'>INTERESTED IN APPLYING?</h3>
-          <p style={{fontSize: 20}}>Fill out the application form below!</p>
 
+          <div data-AOS="fade-up">
+            <h3 className='blueHeading'>INTERESTED IN APPLYING?</h3>
+            <p style={{fontSize: 20}}>Fill out the application form below!</p>
 
-          <br></br>
-          <div className='button-53'><a href="https://docs.google.com/forms/d/e/1FAIpQLSdnEp8Hu1wNlYhC_a7YpbnXhV1o_yuy47R_bnJ-f89tX6JphQ/viewform" target='blank' className='scheduleBtnTxt'>APPLY NOW</a></div>
-          <p>Please contact <a href="mailto: info@codeyourchances.com" target='blank' style={{color: "black"}}>info@codeyourchances.com</a> with any questions.</p>
-
+            <br></br>
+            <div className='button-53'><a href="https://docs.google.com/forms/d/e/1FAIpQLSdnEp8Hu1wNlYhC_a7YpbnXhV1o_yuy47R_bnJ-f89tX6JphQ/viewform" target='blank' className='scheduleBtnTxt'>APPLY NOW</a></div>
+            <p>Please contact <a href="mailto: info@codeyourchances.com" target='blank' style={{color: "black"}}>info@codeyourchances.com</a> with any questions.</p>
+          </div>
         </div>
 
       </div>
